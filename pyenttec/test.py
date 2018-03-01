0c012673b279e997f0534550b7b5ea57a3288ac8
general electrix <general.electrix@gmail.com>
2018-03-01 11:13:55 -0800

    Add test config to setup.py and fix offline test.

--- pyenttec/test.py
+++ pyenttec/test.py
@@ -3,12 +3,13 @@ from nose.tools import assert_equal, assert_raises
 from . import DMXConnectionOffline, DMXAddressError
 
 def test_offline_port():
-    port = DMXConnectionOffline(univ_size=10)
+    univ_size = 24
+    port = DMXConnectionOffline(univ_size=univ_size)
 
-    assert_equal(10, port.dmx_frame)
+    assert_equal(univ_size, len(port.dmx_frame))
     assert all(chan == 0 for chan in port.dmx_frame)
 
     port.set_channel(0, 1)
     assert_equal(1, port.dmx_frame[0])
 
-    assert_raises(DMXAddressError, port.set_channel, 11, 0)
+    assert_raises(DMXAddressError, port.set_channel, univ_size, 0)
--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ with open('README.md') as f:
     readme = f.read()
 
 requires = ['pyserial']
+tests_require = ['nose']
 
 setup(
     name='pyenttec',
     install_requires=requires,
+    tests_require=tests_require,
+    test_suite="nose.collector",
     version='1.0',
     author='Chris Macklin',
     author_email='chris@imaginaryphotons.com',
