1187c513d5cfb9a5f004e586122e2c6fa988e2db
Will Stott <willstott101@gmail.com>
2018-03-06 13:57:41 +0000

    Fix tests, seperate item accessor tests.

--- pyenttec/test.py
+++ pyenttec/test.py
@@ -1,5 +1,5 @@
 """Tests for offline DMX port."""
-from nose.tools import assert_equal, assert_raises
+from nose.tools import assert_equal, assert_raises, raises
 from . import DMXConnectionOffline, DMXAddressError, DMXOverflowError
 
 def get_test_port(univ_size=24):
@@ -14,10 +14,22 @@ def test_offline_port():
 
     port.set_channel(0, 1)
     assert_equal(1, port.dmx_frame[0])
-    assert_equal(1, port[0])
 
     assert_raises(DMXAddressError, port.set_channel, univ_size, 0)
-    assert_raises(DMXAddressError, lambda c, v: port[c] = v, univ_size, 0)
+
+def test_port_item_accessors():
+    univ_size = 24
+    port = get_test_port(univ_size)
+    port[univ_size - 1] = 2
+    assert_equal(2, port.dmx_frame[univ_size - 1])
+    port[0] = 2
+    assert_equal(2, port.dmx_frame[0])
+
+@raises(DMXAddressError)
+def test_port_setitem_max_address():
+    univ_size = 24
+    port = get_test_port(univ_size)
+    port[univ_size] = 2
 
 def test_dmx_value_range():
     port = get_test_port()
