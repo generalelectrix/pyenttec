b21e26ba568da0bd38cd248b45c1a441645b6895
Will Stott <willstott101@gmail.com>
2018-03-06 13:42:02 +0000

    Split up the tests a bit

--- pyenttec/test.py
+++ pyenttec/test.py
@@ -2,9 +2,12 @@
 from nose.tools import assert_equal, assert_raises
 from . import DMXConnectionOffline, DMXAddressError, DMXOverflowError
 
+def get_test_port(univ_size=24):
+    return DMXConnectionOffline(univ_size=univ_size)
+
 def test_offline_port():
     univ_size = 24
-    port = DMXConnectionOffline(univ_size=univ_size)
+    port = get_test_port(univ_size)
 
     assert_equal(univ_size, len(port.dmx_frame))
     assert all(chan == 0 for chan in port.dmx_frame)
@@ -14,6 +17,18 @@ def test_offline_port():
 
     assert_raises(DMXAddressError, port.set_channel, univ_size, 0)
 
+def test_dmx_value_range():
+    port = get_test_port()
+
     assert_raises(DMXOverflowError, port.set_channel, 1, -1)
+
+    port.set_channel(0, 0)
+    assert_equal(0, port.dmx_frame[0])
+
+    port.set_channel(0, 128)
+    assert_equal(128, port.dmx_frame[0])
+
+    port.set_channel(0, 255)
+    assert_equal(255, port.dmx_frame[0])
 
     assert_raises(DMXOverflowError, port.set_channel, 5, 256)
